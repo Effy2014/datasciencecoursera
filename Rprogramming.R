b4c1c6d6aaa24292d0eb71f37fab8b60f02c1004
Xinli Wang <XW@XinlideMacBook-Pro.local>
2015-09-22 23:25:51 -0400

    R programming assignment

--- Rprogramming.R
+++ Rprogramming.R
@@ -1,2 +1,14 @@
 dataset <- read.csv("/Users/XW/Downloads/hw1_data.csv", header = TRUE )
 names(dataset)
+head(dataset,2)
+nrow(dataset)
+dataset[47,]
+sum(is.na(dataset[,1]))
+x <- dataset[,1]
+mean(x = x[!is.na(x)])
+sub <- subset(dataset, Ozone > 31 & Temp > 90)
+summary(sub)
+sub1 <- subset(dataset, Month == 6)
+summary(sub1)
+sub2 <- subset(dataset, Month == 5)
+summary(sub2)
